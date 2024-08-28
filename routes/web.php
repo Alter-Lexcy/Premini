0e833351fc900b3c300e568f03bca1f8e774f4f4
Faizal <144884174+Alter-Lexcy@users.noreply.github.com>
2024-08-29 01:00:33 +0700

    menambahkan Table sponsor

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\SponsorController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,4 +10,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::middleware(['auth'])->group(function(){
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::resource('Sponsor',[SponsorController::class]);
+});
