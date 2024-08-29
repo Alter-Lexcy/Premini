48f5523d198084dc08c557ffdddc2d48435010a2
patt11111 <vanpatt946@gmail.com>
2024-08-29 11:39:25 +0700

    view updete

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/Sponsor';
+    protected $redirectTo = '/sponsors';
 
     /**
      * Create a new controller instance.
