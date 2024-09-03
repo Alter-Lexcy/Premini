fe95f15ea65b7f68f90cc027d6258b5665b8eea6
patt11111 <vanpatt946@gmail.com>
2024-09-03 12:29:43 +0700

    aa

--- app/Models/venue.php
+++ app/Models/venue.php
@@ -1,28 +1,16 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class venue extends Model
+class Venue extends Model
 {
     use HasFactory;
 
-    /**
-     * The table associated with the model.
-     *
-     * @var string
-     */
-    protected $table = 'venues';
+    protected $table = 'venue';
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
         'NamaPembuatEvent',
-    ];
+    ]; 
 }
-
